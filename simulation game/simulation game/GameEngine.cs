a1b9188570e8a7156d50c727a12a06c88d452245
Christopher <chrisrad00x@gmail.com>
2019-10-07 16:18:43 +0200

    Update GameEngine.cs

--- simulation game/simulation game/GameEngine.cs
+++ simulation game/simulation game/GameEngine.cs
@@ -17,11 +17,11 @@ namespace simulation_game
         {
             foreach(Unit unit in world.players)
             {
-                if (unit.IsDead) { continue; }
+                if (unit.IsDead) { continue; }//if the unit is dead, dont waist your bread.....
 
-                unit.NearestEnemy(world.players);
+                unit.NearestEnemy(world.players);//find a unit
 
-                if(unit.ClosestUnit == null)
+                if (unit.ClosestUnit == null)
                 {
                     gameOver = true;
                     winningFaction = unit.Team;
@@ -30,17 +30,17 @@ namespace simulation_game
                 }
 
                 double healthPercent = unit.Health / unit.MaxHealth * 100;
-                if (healthPercent <= 25)
+                if (healthPercent <= 25)//if the unit is low on health it should run away
                 {
                     unit.RandomMove();
                 }
-                else if (unit.WithinRange())
+                else if (unit.WithinRange())//if the closest unit is in attack range, attack it
                 {
                     unit.Attack(unit.ClosestUnit);
                 }
                 else
                 {
-                    unit.Move();
+                    unit.Move();//if they cant reach their enemy then they must move closer to their enemy.
                 }
 
                 world.UpdateWorld();
